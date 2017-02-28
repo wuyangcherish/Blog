ee8fcdf37e6120203bc38dca30b44095fd446b1c
yang.wu <yang.wu@ctrip.com>
2017-02-28 09:54:45 +0800

    add extension app example

---
+++ extension/app/js/todolist.js
@@ -0,0 +1,36 @@
+new Vue({
+	el: '#todo-list',
+	data: {
+		newTodoText: "",
+		todos: [
+			"Do the dishes",
+			"Take out the trash",
+			"Nothing to do "
+		]
+	},
+	methods: {
+		addNewTodo: function() {
+			this.todos.unshift(this.newTodoText);
+			this.newTodoText = '';
+			chrome.storage.sync.set({
+				'todolist': this.todos
+			})
+		},
+		init: function() {
+			var self = this;
+			chrome.storage.sync.get('todolist', function(data) {
+				// console.log(data)  //object  1>todolist[]
+				self.todos = data.todolist
+			})
+		},
+		del: function(index) {
+			this.todos.splice(index, 1);
+			chrome.storage.sync.set({
+				'todolist': this.todos
+			})
+		}
+	},
+	created: function() {
+		this.init();
+	}
+})
